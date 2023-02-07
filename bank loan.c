9b77cb774bf0fcf879830eb28c3cc163a39f9b3c
chokodindo <122534080+chokodindo@users.noreply.github.com>
2023-02-08 02:05:45 +0300

    Add files via upload

---
+++ bank loan.c
@@ -0,0 +1,14 @@
+//program to issue bank loan
+#include <stdio.h>
+int main()
+{
+  int age,salary;
+  printf ("enter age in years and salary:");
+  scanf ("%d%d",&age,&salary);
+  if (age>=21,salary>=21000){
+    printf ("congrats you qualify for a loan");} 
+  else
+  {printf ("unfortunately you don't qualify for a loan");}
+  return 0;
+}
+  
