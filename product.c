9e9dcbb9544f028f896fe5ed42377bf1e66a59ff
chokodindo <122534080+chokodindo@users.noreply.github.com>
2023-03-02 00:34:37 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,16 @@
+//function of product of two numbers
+#include<stdio.h>
+int product 0;
+int main 0
+  {
+  int answer;
+  answer=product 0:
+    printf("\nthe product of two numbers is:%d",answer);
+  return 0;
+  }
+int product 0
+{
+  int a=5,b=4;
+  return a*b;
+}
+  
